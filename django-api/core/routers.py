9ecbd12d85948c125e27d5078957b08cc9b23d9e
Addisu <addisuhaile87@gmail.com>
2023-09-18 10:38:59 +0300

    Install simplejwt and start new auth app

--- django-api/core/routers.py
+++ django-api/core/routers.py
@@ -1,10 +1,13 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-
+from core.auth.viewsets.register import RegisterViewSet
 
 router = routers.SimpleRouter()
 
-# User
+# AUTH
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+
+# USER
 
 router.register(r'user', UserViewSet, basename='user')
 
