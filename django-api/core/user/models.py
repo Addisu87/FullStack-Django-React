7c487550624671ffb6e7242e6653c8e4d7628985
Addisu <addisuhaile87@gmail.com>
2023-09-19 12:06:11 +0300

    Make code DRY by creating abstract

--- django-api/core/user/models.py
+++ django-api/core/user/models.py
@@ -1,17 +1,11 @@
-import uuid
+
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.http import Http404
+
+from core.abstract.models import AbstractModel, AbstractManager
 
 
-class UserManager(BaseUserManager):
-    def get_object_by_public_id(self, public_id):
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except (ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
+class UserManager(BaseUserManager, AbstractManager):
 
     def create_user(self, username, email, password=None, **kwargs):
         """ Create and return a `User` with and email, phone number, username, and password. """
@@ -47,9 +41,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(
-        db_index=True, unique=True, default=uuid.uuid4, editable=False)
+class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -60,9 +52,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True)
-
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
