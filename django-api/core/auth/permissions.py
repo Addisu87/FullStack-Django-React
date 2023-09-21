cd758ac262d138ca3d32acd06a4f98014fb59992
Addisu <addisuhaile87@gmail.com>
2023-09-21 12:58:28 +0300

    Update User permissions and add delete, update comment actions

--- django-api/core/auth/permissions.py
+++ django-api/core/auth/permissions.py
@@ -8,11 +8,18 @@ class UserPermission(BasePermission):
 
         if view.basename in ["post"]:
             return bool(request.user and request.user.is_authenticated)
+
+        if view.basename in ["post-comment"]:
+            if request.method in ['DELETE']:
+                return bool(request.user.is_superuser or request.user in [obj.author, obj.post.author])
+
+            return bool(request.user and request.user.is_authenticated)
         return False
 
     def has_permission(self, request, view):
         if view.basename in ["post"]:
             if request.user.is_anonymous:
                 return request.method in SAFE_METHODS
+
             return bool(request.user and request.user.is_authenticated)
         return False
