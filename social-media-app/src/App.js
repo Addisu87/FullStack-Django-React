72fc7584a5926ca5970d9b262a46bf8596372ce5
Addisu <addisuhaile87@gmail.com>
2023-10-05 12:56:23 +0300

    Create Registration and login forms

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login/" element={<Login />} />
+      <Route path="/register/" element={<Registration />} />
     </Routes>
   );
 }
