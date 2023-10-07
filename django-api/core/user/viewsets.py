b4016d76808b8185a8471037e24fde83b702ef33
Addisu <addisuhaile87@gmail.com>
2023-10-07 10:14:08 +0300

    Update user viewset

--- django-api/core/user/viewsets.py
+++ django-api/core/user/viewsets.py
@@ -18,7 +18,5 @@ class UserViewSet(AbstractViewSet):
 
     def get_object(self):
         obj = User.objects.get_object_by_public_id(self.kwargs['pk'])
-
         self.check_object_permissions(self.request, obj)
-
         return obj
