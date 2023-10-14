4bfa3d83f5419819da889988a7746107096f2206
Addisu <addisuhaile87@gmail.com>
2023-10-14 22:35:28 +0300

    Add default render json

--- django-api/CoreRoot/settings.py
+++ django-api/CoreRoot/settings.py
@@ -149,6 +149,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 15
