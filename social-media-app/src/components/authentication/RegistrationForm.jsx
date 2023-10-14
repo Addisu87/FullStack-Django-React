8ad4975641451f10b841185e7a77362499109f43
Addisu <addisuhaile87@gmail.com>
2023-10-14 16:13:21 +0300

    Update registration form

--- social-media-app/src/components/authentication/RegistrationForm.jsx
+++ social-media-app/src/components/authentication/RegistrationForm.jsx
@@ -176,6 +176,7 @@ const RegistrationForm = () => {
               name="password"
               className="text-xs placeholder-gray-500 pl-10 pr-4 rounded-2xl border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
               placeholder="Enter your password"
+              autoComplete="password"
               {...register("password")}
             />
             {errors.password && (
