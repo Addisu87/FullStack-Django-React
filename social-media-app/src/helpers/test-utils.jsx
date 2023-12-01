088970fac73d62d2f79916212445598ac7564189
Addisu <addisuhaile87@gmail.com>
2023-12-01 10:32:19 +0300

    Modify test-utils file

--- social-media-app/src/helpers/test-utils.jsx
+++ social-media-app/src/helpers/test-utils.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { render as rtlRender } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import store from "../redux/store";
 import { Provider } from "react-redux";
 
 function render(ui, { ...renderOptions } = {}) {
   const Wrapper = ({ children }) => (
-    <BrowserRouter>
+    <MemoryRouter>
       <Provider store={store}>{children}</Provider>
-    </BrowserRouter>
+    </MemoryRouter>
   );
 
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
