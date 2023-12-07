054ffb5858d9b0d7a36590b71fd43846f57350de
Addisu <addisuhaile87@gmail.com>
2023-12-07 10:53:43 +0300

    Modified comment fixtures

--- social-media-app/src/helpers/fixtures/comment.js
+++ social-media-app/src/helpers/fixtures/comment.js
@@ -1,18 +1,16 @@
 import { v4 as uuid4 } from "uuid";
 import { faker } from "@faker-js/faker";
 import userFixtures from "./user";
-import postFixtures from "./post";
 
 const commentFixtures = (
   isLiked = true,
   isEdited = false,
-  user = undefined,
-  post = undefined
+  user = undefined
 ) => {
   return {
     id: uuid4(),
+    post: uuid4(),
     author: user || userFixtures(),
-    post: post || postFixtures(),
     body: faker.lorem.sentence(20),
     edited: isEdited,
     liked: isLiked,
