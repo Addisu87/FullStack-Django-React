de3c335f930e0969e5e28e92daa9d54f76112e98
Addisu <addisuhaile87@gmail.com>
2023-12-18 17:50:42 +0300

    Create new .env.example file

--- social-media-app/src/index.js
+++ social-media-app/src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
+import App from "./App";
 import store from "./redux/store";
 import "./index.css";
-import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
