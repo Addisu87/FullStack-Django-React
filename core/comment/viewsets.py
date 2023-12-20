06cd0c93d9257b3ec4885051d53f7c8dea0ec580
Addisu <addisuhaile87@gmail.com>
2023-12-20 15:51:39 +0300

    Add caching for comment and post viewsets

--- core/comment/viewsets.py
+++ core/comment/viewsets.py
@@ -3,6 +3,7 @@ from django.http.response import Http404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
+from django.core.cache import cache
 
 from core.abstract.viewsets import AbstractViewSet
 from core.comment.models import Comment
@@ -29,6 +30,22 @@ class CommentViewSet(AbstractViewSet):
         obj = Comment.objects.get_object_by_public_id(self.kwargs['pk'])
         self.check_object_permissions(self.request, obj)
         return obj
+
+    # adding caching for comments
+    def list(self, request, *args, **kwargs):
+        comment_objects = cache.get("comment_objects")
+
+        if comment_objects is None:
+            comment_objects = self.filter_queryset(self.get_queryset())
+            cache.set("comment_objects", comment_objects)
+
+        page = self.paginate_queryset(comment_objects)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(comment_objects, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
+from django.core.cache import cache
 
 from core.abstract.viewsets import AbstractViewSet
 from core.post.models import Post
@@ -12,6 +13,7 @@ class PostViewSet(AbstractViewSet):
     http_method_names = ('post', 'get', 'put', 'delete')
     permission_classes = (UserPermission,)
     serializer_class = PostSerializer
+    filter_fields = ["author_public_id"]
 
     def get_queryset(self):
         return Post.objects.all()
@@ -20,6 +22,22 @@ class PostViewSet(AbstractViewSet):
         obj = Post.objects.get_object_by_public_id(self.kwargs['pk'])
         self.check_object_permissions(self.request, obj)
         return obj
+
+    # Adding caching for posts
+    def list(self, request, *args, **kwargs):
+        post_objects = cache.get("post_objects")
+
+        if post_objects is None:
+            post_objects = self.filter_queryset(self.get_queryset())
+            cache.set("post_objects", post_objects)
+
+        page = self.paginate_queryset(post_objects)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(post_objects, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -33,7 +51,7 @@ class PostViewSet(AbstractViewSet):
         user = self.request.user
 
         user.like_post(post)
-        serializer = self.serializer_class(post)
+        serializer = self.serializer_class(post, context={"request": request})
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -43,6 +61,6 @@ class PostViewSet(AbstractViewSet):
         user = self.request.user
 
         user.remove_like_post(post)
-        serializer = self.serializer_class(post)
+        serializer = self.serializer_class(post, context={"request": request})
 
         return Response(serializer.data, status=status.HTTP_200_OK)
