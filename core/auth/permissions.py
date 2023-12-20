75654ad73a8f484641c8a6f8e0e69898979acb78
Addisu <addisuhaile87@gmail.com>
2023-12-20 10:50:48 +0300

    Modified Docerfile, permissions, and workflows

--- core/auth/permissions.py
+++ core/auth/permissions.py
@@ -23,7 +23,7 @@ class UserPermission(BasePermission):
         return False
 
     def has_permission(self, request, view):
-        if view.basename in ["post", "post-comment" "user", "auth-logout"]:
+        if view.basename in ["post", "post-comment", "user", "auth-logout"]:
             if request.user.is_anonymous:
                 return request.method in SAFE_METHODS
 
